2c938496563f967e0ceddf44b6da72137b439a78
Dominic May <jack.thatch@gmail.com>
2013-11-27 00:07:44 +0800

    Allow users to specify only an input filename, we generate the output filename

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,20 @@ int main(int argc, char** argv)
         in = args[0];
 
         // We use the second argument as the output filename.
-        out = args[1];
+
+        if (args.size() == 1){
+             // If the output filename is unspecified, use a modified form of
+             // the input filename.
+             //
+             // e.g. "jupiter boot.asm" is equivalent to "jupiter boot.asm boot.o"
+
+             // Strip the last file extension, and replace with ".o"
+             out = in.substr(0, in.find_last_of('.')) + ".o";
+
+        } else {
+            // Otherwise, we use the second argument as the output filename.
+            out = args[1];
+        }
     }
 
     /// READ IN THE INPUT FILE
