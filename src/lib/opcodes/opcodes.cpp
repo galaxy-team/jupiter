24b3a5a8cbef8eb63260e63dbac4fea7ce6d853b
Dominic May <jack.thatch@gmail.com>
2013-11-27 00:13:37 +0800

    A few Opcode implementations

---
+++ src/lib/opcodes/opcodes.cpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+#include <string>
+
+#include "opcodes.hpp"
+
+std::string galaxy::jupiter::opcodes::Opcode::makeRepr(std::string vars){
+    return "<" + getType() + " " + vars + ">";
+}
+
+std::string galaxy::jupiter::opcodes::LabelOpcode::getType(){ return "LabelOpcode"; };
+std::string galaxy::jupiter::opcodes::LabelOpcode::repr(){
+    return makeRepr("label=" + label);
+}
+
+std::uint16_t galaxy::jupiter::opcodes::InstructionOpcode::assemble(){ return 0;}
+std::string galaxy::jupiter::opcodes::InstructionOpcode::getType(){ return "InstructionOpcode"; }
+std::string galaxy::jupiter::opcodes::InstructionOpcode::repr(){
+    return makeRepr("name=\"" + name + "\",a=\"" + a + "\",b=\"" + b + "\"");
+}
+
+std::string galaxy::jupiter::opcodes::OrigOpcode::getType(){ return "OrigOpcode"; }
+std::string galaxy::jupiter::opcodes::OrigOpcode::repr(){
+    return makeRepr("location=" + std::to_string(location));
+}
---
+++ src/lib/opcodes/opcodes.hpp
@@ -0,0 +1,62 @@
+#include <cstdint>
+#include <string>
+
+#ifndef _OPCODES_HPP
+#define _OPCODES_HPP
+
+namespace galaxy {
+    namespace jupiter {
+        namespace opcodes {
+            // std::unordered_map<std::string,int> galaxy::jupiter::opcodes::basic_opcodes;
+
+            class Opcode {
+            public:
+                virtual ~Opcode() {}
+                virtual std::string getType() = 0;
+                virtual std::string repr() = 0;
+
+                std::string makeRepr(std::string vars);
+            };
+
+            class LabelOpcode : public Opcode {
+            public:
+                LabelOpcode(std::string _label) {
+                    label = _label;
+                };
+                std::string label = "";
+
+                std::string getType();
+                std::string repr();
+            };
+
+            class InstructionOpcode : public Opcode {
+            public:
+                InstructionOpcode(std::string _name, std::string _a, std::string _b) {
+                    name = _name;
+                    a = _a;
+                    b = _b;
+                };
+                std::string name = "";
+                std::string a = "";
+                std::string b = "";
+
+                std::uint16_t assemble();
+                std::string getType();
+                std::string repr();
+            };
+
+            class OrigOpcode : public Opcode {
+            public:
+                OrigOpcode(int location) : location(location) {};
+                int location;
+
+                std::string getType();
+                std::string repr();
+            };
+
+
+        }
+    }
+}
+
+#endif
