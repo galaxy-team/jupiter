9849ac74cce9971b8f8d8340d5880fd14b69ed99
Dominic May <jack.thatch@gmail.com>
2013-11-29 15:13:42 +0800

    If a Token does contain a newline, it will only ever be a single newline

--- src/lib/tokenizer/token.cpp
+++ src/lib/tokenizer/token.cpp
@@ -5,13 +5,9 @@
 std::string galaxy::jupiter::Token::repr(){
     std::string reprd_contents = contents;
 
-    if (reprd_contents.find("\n") >= 0) {
-        while(reprd_contents.find("\n") != -1){
-            reprd_contents = reprd_contents.replace(
-                reprd_contents.find("\n"),
-                1, "\\n"
-            );
-        }
+
+    if (reprd_contents == "\n"){
+        reprd_contents = "\\n";
     }
 
     return "<Token:" + name + " \"" + reprd_contents + "\">";
