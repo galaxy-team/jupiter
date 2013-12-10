5c78bad86db219578af016c552d562220d88b9af
Dominic May <jack.thatch@gmail.com>
2013-12-10 23:02:47 +0800

    Use a wrapper around assemble
    The wrapper outputs std::bitset instances instead of std::uint16_t's.
    This forces the catch test framework to output the numbers in binary,
    instead of as decimals.
    
    In other tests other tests I might use a hex wrapper. We'll see

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,7 +1,27 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include <sstream>
+#include <bitset>
 #include "libjupiter.hpp"
+
+std::vector<std::bitset<16>> bitset_assemble(
+    std::string::const_iterator begin,
+    std::string::const_iterator end
+)
+{
+    auto objfile = galaxy::jupiter::assemble(begin, end);
+    std::vector<std::bitset<16>> bitset_repres;
+    for (auto it: objfile.object_code) {
+        bitset_repres.push_back(it);
+    }
+    return bitset_repres;
+}
+
+void REQUIRE_ASM(std::string input_string, std::vector<std::bitset<16>> vector_required) {
+    auto objfile = bitset_assemble(input_string.begin(), input_string.end());
+    REQUIRE(objfile == vector_required);
+}
 
 TEST_CASE("Initialization", "") {
     std::string input = "ADD A,1\n";
@@ -12,8 +32,8 @@ TEST_CASE("Initialization", "") {
 TEST_CASE("Test basic opcodes", "[basic_opcode]") {
     std::string input = "ADD A,1\nSUB A,1\n";
 
-    auto objfile = galaxy::jupiter::assemble(input.begin(), input.end());
-
-    REQUIRE(objfile.object_code.at(0) == 0x8801);
-    REQUIRE(objfile.object_code.at(1) == 0x8803);
+    REQUIRE_ASM(input, {
+        0x8801,
+        0x8803
+    });
 }
