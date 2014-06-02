477ddd9dc1e46dd7efbc0420b288b5fa5a6dd5b2
Dominic May <jack.thatch@gmail.com>
2014-06-02 06:45:17 +0000

    Have an explicit method rather than via the constructor

--- src/lib/tokenizer/token.hpp
+++ src/lib/tokenizer/token.hpp
@@ -10,19 +10,21 @@ public:
     int token_type;
     std::uint16_t line_no;
     std::string contents;
-    std::string token_name;
+    std::string token_name = "";
 
     token(int token_type, std::uint16_t line_num, std::string contents) :
         token_type(token_type),
         line_no(line_num + 1),
-        contents(contents),
-        token_name("") {};
+        contents(contents) {};
 
-    token(int token_type, std::uint16_t line_num, std::string contents, std::string token_name) :
+    token(int token_type, std::uint16_t line_num, token_vector contents) :
         token_type(token_type),
         line_no(line_num + 1),
-        contents(contents),
-        token_name(token_name) {};
+        contents_vector(contents) {};
+
+    void set_token_name(std::string token_name) {
+        this->token_name = token_name;
+    }
 
     std::string repr();
     std::string normalized();
