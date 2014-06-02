25b49f3da062bf7e1ed15a341a21cd1b3672515c
Dominic May <jack.thatch@gmail.com>
2014-06-02 06:49:48 +0000

    Cleanup previously moved code

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,27 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include <sstream>
-#include <bitset>
 #include "libjupiter.hpp"
-
-std::vector<std::bitset<16>> bitset_assemble(
-    std::string::const_iterator begin,
-    std::string::const_iterator end
-)
-{
-    auto objfile = galaxy::jupiter::assemble(begin, end);
-    std::vector<std::bitset<16>> bitset_repres;
-    for (auto it: objfile.object_code) {
-        bitset_repres.push_back(it);
-    }
-    return bitset_repres;
-}
-
-void REQUIRE_ASM(std::string input_string, std::vector<std::bitset<16>> vector_required) {
-    auto objfile = bitset_assemble(input_string.begin(), input_string.end());
-    REQUIRE(objfile == vector_required);
-}
 
 TEST_CASE("Initialization", "") {
     std::string input = "ADD A,1\n";
@@ -37,20 +17,6 @@ TEST_CASE("Test basic opcodes", "[basic_opcode]") {
         0x8803
     });
 }
-
-#include "rl_parser/rl_parser.hpp"
-#include "rl_parser/parser.h"
-
-#define T(token_type, contents) \
-    new galaxy::jupiter::Token(token_type, contents)
-
-#define P(token_type, contents) \
-    new galaxy::jupiter::opcodes::Part(\
-        T(token_type, contents)  \
-    )
-
-#define REQUIRE_OPCODES(a, b) REQUIRE(a->repr() == a->repr())
-
 TEST_CASE("Test basic parsing", "[parsing]") {
     std::string input = "ADD A, A";
 
